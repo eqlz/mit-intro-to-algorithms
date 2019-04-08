2c4622f490feaa3d283131144b5899b495427c29
Emily Zhang <qinglu.zh@gmail.com>
2019-04-08 19:49:27 -0400

    Get left and right node index given an index

--- heapSort.js
+++ heapSort.js
@@ -1,13 +1,13 @@
 var a = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1];
 
-const maxHeapify = (arrayToMaxHeapify, indexOfLargestValue) => {
+const maxHeapify = (arrayToMaxHeapify, index) => {
 
 }
 
-const getIndexOfLeftChild = () => {
-
+const getIndexOfLeftChild = (index) => {
+    return (index * 2) + 1
 }
 
-const getIndexOfRightChild = () => {
-
+const getIndexOfRightChild = (index) => {
+    return (index * 2) + 2
 }
