d7066ff52c75813c714967b1cfdceb7bb34f31fd
Emily Zhang <qinglu.zh@gmail.com>
2019-04-07 22:24:34 -0400

    Set up for max heapify function

---
+++ heapSort.js
@@ -0,0 +1,13 @@
+var a = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1];
+
+const maxHeapify = (arrayToMaxHeapify, indexOfLargestValue) => {
+
+}
+
+const getIndexOfLeftChild = () => {
+
+}
+
+const getIndexOfRightChild = () => {
+
+}
