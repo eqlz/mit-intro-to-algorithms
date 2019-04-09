cd535ac8212d15839dd19c8fe23b244f04a1452e
Emily Zhang <qinglu.zh@gmail.com>
2019-04-08 20:19:49 -0400

    Implement maxHeapify to maintain max-heap property

--- heapSort.js
+++ heapSort.js
@@ -1,6 +1,31 @@
 var a = [16, 4, 10, 14, 7, 9, 3, 2, 8, 1];
 
 const maxHeapify = (arrayToMaxHeapify, index) => {
+    let heapSize = arrayToMaxHeapify.length;
+
+    let indexOfLeftChildNode = getIndexOfLeftChild(index);
+    let indexOfRightChildNode = getIndexOfRightChild(index);
+
+    let indexOfLargestValue;
+    if(indexOfLeftChildNode < heapSize && arrayToMaxHeapify[indexOfLeftChildNode] > arrayToMaxHeapify[index]) {
+        indexOfLargestValue = indexOfLeftChildNode;
+    } else {
+        indexOfLargestValue = index;
+    }
+
+    if(indexOfRightChildNode < heapSize && arrayToMaxHeapify[indexOfRightChildNode] > arrayToMaxHeapify[indexOfLargestValue]) {
+        indexOfLargestValue = indexOfRightChildNode;
+    }
+
+    if(indexOfLargestValue !== index) {
+        let largestValue = arrayToMaxHeapify[indexOfLargestValue];
+        let indexValue = arrayToMaxHeapify[index];
+
+        arrayToMaxHeapify[index] = largestValue;
+        arrayToMaxHeapify[indexOfLargestValue] = indexValue;
+
+        maxHeapify(arrayToMaxHeapify, indexOfLargestValue);
+    }
 
 }
 
@@ -11,3 +36,6 @@ const getIndexOfLeftChild = (index) => {
 const getIndexOfRightChild = (index) => {
     return (index * 2) + 2
 }
+
+maxHeapify(a, 1);
+console.log('result: ', a);
