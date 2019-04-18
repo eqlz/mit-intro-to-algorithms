ef9740105e19436af00f48db8d092ea718f95b9f
Emily Zhang <qinglu.zh@gmail.com>
2019-04-17 21:47:39 -0400

    Add explanation to arrays

--- multiArrayRepresentation.js
+++ multiArrayRepresentation.js
@@ -13,3 +13,12 @@ list.add(1);
 let key = [9, 16, 4, 1];
 let prev = [-1 , 0, 1, 2];
 let next = [1, 2, 3, -1];
+
+// explanation
+/*
+  Take first item in array key, 9, it's next is 16 and 16's index in key array is 1.
+  so in next array, first item will 1.
+  
+  prev and next are arrays of indice of items in key array
+
+*/
