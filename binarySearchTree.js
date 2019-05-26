9e6cd9d8e610dbf0a51cbef2b06dcd00ead106a7
Emily Zhang <qinglu.zh@gmail.com>
2019-05-26 17:27:43 -0400

    Add method to insert a value in binary search tree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -2,11 +2,39 @@ const root = Symbol('root');
 
 class BinarySearchTree {
   constructor() {
-    this[root] = root;
+    this[root] = null;
   }
 
-  add(data) {
+  insert(data) {
+    let newNode = new BinarySearchTreeNode(data);
     
+    if(this[root] === null) { // when binary search tree is empty
+      this[root] = newNode;
+    }
+    else {
+      let parentNode = this[root];
+
+      while(parentNode !== null) { // find the node in correct left or right sub-tree to add newNode 
+        if(newNode.data < parentNode.data) {
+          parentNode = parentNode.left;
+        } 
+        else {
+          parentNode = parentNode.right;
+        }
+      }
+
+      newNode.parent = parentNode;
+      if(newNode.data < parentNode.data) {
+        parentNode.left = newNode;
+      }
+      else {
+        parentNode.right = newNode;
+      }
+    }
+  }
+
+  delete() {
+
   }
 }
 
