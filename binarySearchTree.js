6bf28167e5562daa6feeceaea8f5e7ff63010516
Emily Zhang <qinglu.zh@gmail.com>
2019-06-02 13:45:50 -0400

    handle BST deletion cases of havng no child node and one child node

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -33,8 +33,39 @@ class BinarySearchTree {
     }
   }
 
-  delete() {
+  delete(nodeToDelete) {
+    if(nodeToDelete.left === null) { // left child is null or both left and right are null
+      this.transplant(nodeToDelete, nodeToDelete.right);
+    }
+    else if(nodeToDelete.right === null) { // left child is not null, right child is null
+      this.transplant(nodeToDelete, nodeToDelete.left);
+    }
+    else { // neither left child nor right child is null
+      // find a node:
+      // - in right sub-tree of nodeToDelete
+      // - whose left child is null
+      
+      // TO BE CONTINUED
+    }
+  }
 
+  transplant(oldNode, newNode) {
+    if(oldNode.parent === null) { 
+      this[root] = newNode;
+    }
+    // find oldNode's parent, decide if oldNode is a left or right child
+    // from oldNode's parent perspective to replace oldNode with newNode
+    else if(oldNode === oldNode.parent.left) { 
+      oldNode.parent.left = newNode;
+    }
+    else {
+      oldNode.parent.right = newNode;
+    }
+
+    // from newNode's perspective, to replace its parent with oldNode's parent
+    if(newNode !== null) {
+      newNode.parent = oldNode.parent;
+    }
   }
 }
 
